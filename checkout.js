05773c33bed73ac42260a666f0d3227164d96856
kajzuz <kajsauzelac@gmail.com>
2023-02-02 18:59:14 +0100

    More more changes

--- checkout.js
+++ checkout.js
@@ -7,57 +7,47 @@ const bodyCheckouteEl = document.getElementById("bodyCheckout");
 const buyButtonEl = document.getElementById("buyButton");
     
   
-//Printing out all of my saved products in localstore on the checkout site
+//Printing out all of my saved products in localstore on the checkout site with total price
+let price = 0;
+
 if (localStorage.products) {
 
 
-    let local = JSON.parse(localStorage.getItem("products")); //Make the minus and plus buttom work
+    let local = JSON.parse(localStorage.getItem("products")); 
 
 
     console.log(local);
+
+    price = 0;
 
     for (let i = 0; i < local.length; i++) {
       
       sectionCEl.innerHTML += `
       <article class=articleCheckout>
-      <b>${local[i].title} </b><br> 
-      <img width=10% src= ${local[i].image} alt=""><br> 
-      <strong>$ ${local[i].price} </strong> <br>
-      <input class="buttonReduce" type="button" value="-" id="reduceButton" onclick="reduce('${""}')">
-      <b></b>
-      <input class="buttonIncrease" type="button" value="+" id="increaseButton" onclick="increase('${""}')">
+      <br>
+      <b>${local[i].title} </b><br><br> 
+      <img width=25% src= ${local[i].image} alt=""><br> 
+      <strong>$ ${local[i].price} </strong> <br><br>
+      <img src="images/trashcanIcon.png" alt="traschcan icon"
+      <input class="buttonDeleteProduct" type="button" value="" id="reduceButton" onclick="deleteProduct()">
       <br><br><br>
       </article>
       `
-      
+      price += parseFloat(local[i].price);
     }
-   
-      
   }
-  
 
-    //For counting all the prices of the products, add this function ta all other funktions
-    //Put this in checkout, make this count!
-    let price = 0;
-
-    function yourTotalCost(){
-
-    price = 0;
-
-    for (let i = 0; i < localStorage.length; i++) {
-        price += parseFloat(localStorage[i].price);
-        
-        
-    }
-
-    }
-    
-
-    sectionPriceEl.innerHTML += ` <h3> Your total price: </h3> ${price.toFixed(2)} <br><br> 
+    sectionPriceEl.innerHTML += ` <h3> Your total price: </h3> <strong class="totalPrice">$ ${price.toFixed(2)} dollar </strong><br><br> 
     <input class="buttonBuy" type="button" value="Buy" id="buyButton" onclick="buyAll()">
     `
+  
+    function deleteProduct() {
+      
+      
 
-    
+    }
+
+    //Message after order is sent
     function buyAll(){
     bodyCheckouteEl.innerHTML = `<h4 class="endText">Thank you for shopping at shoppingflex <br> Your order has been registrered!</h4>` ;
     };
